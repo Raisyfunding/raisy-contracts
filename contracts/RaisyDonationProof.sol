c8c4739eb7175965f1ac1dbf9f2a740bbe2a6157
0xsacha <sacha.graff@icloud.com>
2021-10-25 21:50:32 +0200

    feat: add RaisyDonationProof
    mapping with donation info
    mint function

---
+++ contracts/RaisyDonationProof.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title RaisyNFT
+ * RaisyNFT - ERC721 contract that whitelists a trading address, and has minting functionality.
+ */
+contract Donationproof is ERC721, Ownable {
+    mapping(uint256 => donationInfo) private _donationInfo;
+
+    struct donationInfo {
+        uint256 amount;
+        address tokenUsed;
+        uint256 campaignId;
+        address recipient;
+    }
+
+    event Minted(uint256 tokenId, donationInfo param);
+
+    uint256 private _currentTokenId = 0;
+
+    constructor(string memory _name, string memory _symbol)
+        public
+        ERC721(_name, _symbol)
+    {}
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     */
+
+    function mint(donationInfo calldata params) external override {
+
+        uint256 newTokenId = _getNextTokenId();
+        _safeMint(params.recipient, newTokenId);
+        _donationInfo[newTokenId] = donationInfo(params);
+        _setApprovalForAll(address(0), params.recipient, 1)
+        emit Minted(newTokenId, params);
+    }
+
+    /**
+    @notice Burns a tNFT
+    @dev Only the owner or an approved sender can call this method
+    @param _tokenId the token ID to burn and the mapping too
+    */
+    function burn(uint256 _tokenId) external {
+        address operator = _msgSender();
+        require(
+            ownerOf(_tokenId) == operator
+        );
+
+        // Destroy token mappings
+        _burn(_tokenId);
+        delete (_donationInfo(_tokenId));
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId.add(1);
+    }
+
