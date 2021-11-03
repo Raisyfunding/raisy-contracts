1aedf15f23f11c0928958d7dd8a2230ae031691c
Evolve <Artevolve@yahoo.com>
2021-11-03 13:31:03 +0100

    fix: libs

--- test/unit/Raisytoken_unit_test.js
+++ test/unit/Raisytoken_unit_test.js
@@ -11,32 +11,32 @@ const {
 
 const RaisyToken = artifacts.require("RaisyToken");
 
-contract("RaisyToken", function([owner, projectowner, feeRecipient]) {
+contract("RaisyToken", ([ owner, projectowner, feeRecipient]) => {
 	const MAX_SUPPLY = new BN(ether("10000000"));
 	const OVER_MAX_SUPPLY = new BN(ether("10000001"));
 	const ONE_THOUSAND = new BN(ether("1000"));
 
-	beforeEach(async function() {
+	beforeEach(async () =>  {
 		this.raisyToken = await RaisyToken.new("RaisyToken", "RSY", MAX_SUPPLY, {
 			from: owner,
 		});
 	});
 
-	describe("Mint tokens", function() {
-		it("reverts when caller is not owner", async function() {
+	describe("Mint tokens", () =>  {
+		it("reverts when caller is not owner", async () =>  {
 			await expectRevert(
 				this.raisyToken.mint(owner, ONE_THOUSAND, { from: projectowner }),
 				"Ownable: caller is not the owner"
 			);
 		});
-		it("successfully mints 1000 token", async function() {
+		it("successfully mints 1000 token", async () =>  {
 			await this.raisyToken.mint(owner, ONE_THOUSAND, { from: owner });
 
 			expect(await this.raisyToken.circulatingSupply()).to.be.bignumber.equal(
 				ONE_THOUSAND
 			);
 		});
-		it("reverts when exceed cap", async function() {
+		it("reverts when exceed cap", async () =>  {
 			await expectRevert(
 				this.raisyToken.mint(owner, OVER_MAX_SUPPLY, { from: owner }),
 				"ERC20Capped: cap exceeded"
