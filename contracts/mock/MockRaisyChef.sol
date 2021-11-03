8c24fd5c71a26427d1b6104b4572337e5f6ed246
Matteo <matteo.georges74@hotmail.fr>
2021-11-03 23:22:52 +0100

    feat: mock RaisyChef and tests

---
+++ contracts/mock/MockRaisyChef.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../RaisyChef.sol";
+
+contract MockRaisyChef is RaisyChef {
+    uint256 public blockOverride;
+
+    constructor(
+        RaisyToken _Raisy,
+        address _devaddr,
+        address _daotreasuryaddr,
+        uint256 _rewardPerBlock,
+        uint256 _lockDuration,
+        uint256 _startBlock
+    )
+        RaisyChef(
+            _Raisy,
+            _devaddr,
+            _daotreasuryaddr,
+            _rewardPerBlock,
+            _lockDuration,
+            _startBlock
+        )
+    {}
+
+    function setBlockOverride(uint256 _block) external {
+        blockOverride = _block;
+    }
+
+    function _getBlock() internal view override returns (uint256) {
+        return blockOverride;
+    }
+}
